6dafe5b3d769308f42294bacaaf4f5efa4ebd0e3
tmskrtsz <tamas@tuta.io>
2019-02-20 19:03:37 +0200

    Add fonts to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,14 @@ module.exports = {
         icon: 'src/assets/images/website-icon.png' // This path is relative to the root of the site.
       }
     },
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Muli:400,700']
+        }
+      }
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
   ]

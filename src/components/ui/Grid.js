17695e3e6c9c167354839dfaa610474e940794a9
tmskrtsz <tamas@tuta.io>
2019-02-22 20:44:12 +0200

    Update grid

--- src/components/ui/Grid.js
+++ src/components/ui/Grid.js
@@ -1,24 +1,31 @@
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import theme from '../../theme'
 
 const Container = styled.div`
-  width: ${ props => props.size || props.theme.breakpoints.xl };
-  height: 100%;
+  width: ${ props => (props.size === 'fluid' ? '100%' : props.size || props.theme.breakpoints.xl) };
   margin: 0 auto;
 `
 
 const Row = styled.div`
-  height: 100%;
   display: flex;
-  flex: 1;
   flex-wrap: wrap;
+  flex-direction: ${ props => props.dir || 'row' };
   align-items: ${ props => (props.align ? props.align : null) };
   justify-content: ${ props => (props.justify ? props.justify : null) };
+  ${ ({ grid }) =>
+    grid &&
+    css`
+      & > ${ Column } {
+        flex-basis: ${ (grid * 100).toFixed(2) }%;
+        max-width: ${ (grid * 100).toFixed(2) }%;
+      }
+    ` }
 `
 
 const Column = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: ${ props => props.dir || 'column' };
   align-items: ${ props => (props.align ? props.align : null) };
@@ -26,10 +33,11 @@ const Column = styled.div`
 `
 
 Container.propTypes = {
-  size: PropTypes.oneOf(Object.keys(theme.breakpoints)),
+  size: PropTypes.oneOf(Object.keys(theme.breakpoints).concat(['fluid'])),
 }
 
 Row.propTypes = {
+  grid: PropTypes.number,
   dir: PropTypes.oneOf(['column', 'row']),
   align: PropTypes.oneOf(['flex-start', 'center', 'flex-end']),
   justify: PropTypes.oneOf(['flex-start', 'center', 'flex-end']),

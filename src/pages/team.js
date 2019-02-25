2aa9af5598a479a7eedb2aab212e06d636b12b96
Kollan House <kollanh@gmail.com>
2019-02-24 16:01:40 -0800

    fix missing fields.

--- src/pages/team.js
+++ src/pages/team.js
@@ -55,7 +55,9 @@ export const allTeamQuery = graphql`
             slug
           }
           frontmatter {
+            name
             title
+            image
           }
         }
       }

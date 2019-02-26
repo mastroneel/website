13c4b3fe2d3b58aa537e77a1c76bb8e1c5264021
tmskrtsz <tamas@tuta.io>
2019-02-26 14:39:42 +0200

    Add mouse icon to pageintro

--- src/components/PageIntro.js
+++ src/components/PageIntro.js
@@ -1,34 +1,81 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { css } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 import { rgba } from 'polished'
 
 import { Container, Row, Column } from './ui/Grid'
 import Heading from './ui/Heading'
+import mouse from '../images/mouse.svg'
 
 const Spacing = css`
   padding: 7em 0;
 `
 
 const TextBody = styled.div`
+  max-width: 75%;
+
   h4 {
     margin: 0;
+    padding-top: 0.2em;
+    line-height: 1.2;
+    text-transform: initial;
+    letter-spacing: 0.5px;
   }
 
   p {
-    max-width: 70%;
     color: ${ props => rgba(props.theme.color.dark, 0.8) };
   }
 `
 
 const ImageBody = styled.div`
-  max-width: 70%;
   height: 100%;
 
   img {
     width: 640px;
     height: 100%;
   }
+`
+
+const ScrollAnim = keyframes`
+  0% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+
+  50% {
+    opacity: 0;
+    transform: translateY(0.3em);
+  }
+
+  70% {
+    opacity: 0;
+    transform: translateY(0);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`
+
+const Mouse = styled.div`
+  background-image: url(${ mouse });
+  background-repeat: no-repeat;
+  width: 24px;
+  height: 32px;
+  position: relative;
+`
+
+const Scroll = styled.div`
+  content: '';
+  background-color: ${ props => props.theme.color.pink };
+  width: 4px;
+  height: 7px;
+  border-radius: 10px;
+  position: absolute;
+  top: 5px;
+  left: 43%;
+  animation: ${ ScrollAnim } 2s ease infinite;
 `
 
 const PageIntro = ({ small, heading, paragraph, image }) => (
@@ -44,6 +91,9 @@ const PageIntro = ({ small, heading, paragraph, image }) => (
           </Heading>
           {paragraph && <p>{paragraph}</p>}
         </TextBody>
+        <Mouse>
+          <Scroll />
+        </Mouse>
       </Column>
       {image && (
         <Column align="center">

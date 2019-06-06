1932059a6693b36217326915b1ddeb391a2c1482
Kollan House <kollanh@gmail.com>
2019-06-06 11:01:25 -0700

    missed description.

--- src/templates/page.js
+++ src/templates/page.js
@@ -20,6 +20,7 @@ export default ({ children, pageContext }) => {
     <>
       <SEO
         title={frontmatter.title}
+        description={frontmatter.description}
       />
       <PageIntro
         small={frontmatter.subtitle}
